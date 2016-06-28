bee8632f61307884cbbde843060a8d977fb5ad33
David J. Tedeschi <tedechi15lnc@gmail.com>
2016-06-28 11:40:56 -0400

    add directories now added

---
+++ Landscape/landscape.cc
@@ -0,0 +1,43 @@
+// landscape.cc
+// topographical data
+// Bradley McClain, University of South Carolina
+
+//-----------------------------------------------------------------------
+#include "landscape.hh"
+
+using namespace std;
+
+void landscape() {
+	TFile *f = new TFile("Snolab_2.85_out.root");
+	TH2F *h = (TH2F*)f->Get("landscape2");
+	h->SetDirectory(0);
+	Int_t binx = h->GetNbinsX();
+	Int_t biny = h->GetNbinsY();
+	Double_t min = h->GetMinimum();
+	Double_t max = h->GetMaximum();
+	Int_t binnum = h->FindBin(min/2,max/2);
+	Double_t heigthAt0 = h->GetBinContent(binnum);
+	
+	cout << "number of bins x: " << binx << endl;
+	cout << "number of bins y: " << biny << endl;
+	cout << "min heigth: " << min << endl;
+	cout << "max heigth: " << max << endl;
+	cout << "heigth at (0,0): " << heigthAt0 << endl;
+	
+	//TCanvas *can = new TCanvas("can","landscape",0,0,800,800);
+	//can->cd();
+	//h->Draw();
+	//can->Update();
+	/*
+	char landcan[150];
+		sprintf(landcan,"landcan.pdf");
+		can->Print(landcan,"pdf");
+	*/
+	f->Close();
+	delete f;
+}
+
+int main(int argc, char* argv[]) {
+	landscape();
+	return 0;
+}
---
+++ Landscape/landscape.hh
@@ -0,0 +1,31 @@
+#define landscape_hh
+#include <sstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <fstream>
+#include <list>
+#include <TROOT.h>
+#include <TFile.h>
+#include <TTree.h>
+#include <TH1D.h>
+#include <TH2F.h>
+#include <TF1.h>
+#include <TGraph.h>
+#include <TChain.h>
+#include <TCanvas.h>
+#include <TMath.h>
+#include "TBenchmark.h"
+#include <TColor.h>
+
+#include "TGeoManager.h"
+#include "TGeoMaterial.h"
+#include "TGeoMedium.h"
+#include "TGeoManager.h"
+#include "TPaveText.h"
+#include "TDatabasePDG.h"
+#include "TVirtualGeoTrack.h"
+#include "TVector3.h"
+#include "TApplication.h"
+#include "TGeoCompositeShape.h"
+
