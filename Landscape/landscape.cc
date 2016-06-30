301f0434b263be0d002453fba0e6ef83f05c7ad9
David J. Tedeschi <tedechi15lnc@gmail.com>
2016-06-30 09:19:18 -0400

    using correct topology file now

--- Landscape/landscape.cc
+++ Landscape/landscape.cc
@@ -8,14 +8,14 @@
 using namespace std;
 
 void landscape() {
-	TFile *f = new TFile("Snolab_2.85_out.root");
+	TFile *f = new TFile("../Data/Surf4850_2.85_out.root");
 	TH2F *h = (TH2F*)f->Get("landscape2");
 	h->SetDirectory(0);
 	Int_t binx = h->GetNbinsX();
 	Int_t biny = h->GetNbinsY();
 	Double_t min = h->GetMinimum();
 	Double_t max = h->GetMaximum();
-	Int_t binnum = h->FindBin(min/2,max/2);
+	Int_t binnum = h->FindBin(0,0);
 	Double_t heigthAt0 = h->GetBinContent(binnum);
 	
 	cout << "number of bins x: " << binx << endl;
@@ -25,14 +25,14 @@ void landscape() {
 	cout << "heigth at (0,0): " << heigthAt0 << endl;
 	
 	//TCanvas *can = new TCanvas("can","landscape",0,0,800,800);
-	//can->cd();
-	//h->Draw();
-	//can->Update();
-	/*
-	char landcan[150];
-		sprintf(landcan,"landcan.pdf");
-		can->Print(landcan,"pdf");
-	*/
+//	can->cd();
+//	h->Draw();
+//	can->Update();
+	
+//	char landcan[150];
+//		sprintf(landcan,"landcan.pdf");
+//		can->Print(landcan,"pdf");
+//	
 	f->Close();
 	delete f;
 }
