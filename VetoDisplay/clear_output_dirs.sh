9978f7a525a8ae17fb1d0f036b6120adee4bae49
David J. Tedeschi <tedechi15lnc@gmail.com>
2016-10-19 11:25:38 -0400

    clear_output_dirs.sh

---
+++ VetoDisplay/clear_output_dirs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+
+# clear output directories for VetoDisplay
+rm -f output/2/*
+rm -f output/3/*
+rm -f output/4/*
+rm -f output/5/*
+rm -f output/6/*
+rm -f output/7/*
+rm -f output/8/*
+rm -f output/9/*
+rm -f output/other/*
+rm -f output/plots/*
+
